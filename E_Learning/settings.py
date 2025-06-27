eae669f43d897a5f4714ed4477eb004188e58824
joniyad <j649618@gmail.com>
2025-06-27 10:02:35 +0300

    commit

--- E_Learning/settings.py
+++ E_Learning/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    'https://frontend-z5in.onrender.com',
+    'https://edulearn-6xzb.onrender.com',
 
 ]
 
@@ -206,7 +206,7 @@ DOMAIN=os.environ.get('DOMAIN')
 DOMAIN_REMOTE=os.environ.get('DOMAIN_REMOTE')
 
 
-FRONTEND_URL = os.environ.get('FRONTEND_URL', default='https://frontend-z5in.onrender.com')
+FRONTEND_URL = os.environ.get('FRONTEND_URL', default='https://edulearn-6xzb.onrender.com')
 PAYPAL_TEST = True
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
